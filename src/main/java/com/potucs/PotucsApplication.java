d293351ebb157a9f72752987c93c59d79969b2b1
wangzekun <wangzekun@bonc.com.cn>
2021-07-27 15:34:35 +0800

    提交启动类

---
+++ src/main/java/com/potucs/PotucsApplication.java
@@ -0,0 +1,18 @@
+package com.potucs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auther wangzekun
+ * @Date 2021/7/27 10:50
+ * @Description
+ */
+
+@SpringBootApplication
+public class PotucsApplication {
+    public static void main(String[] args) {
+        //启动spring应用程序
+        SpringApplication.run(PotucsApplication.class,args);
+    }
+}
